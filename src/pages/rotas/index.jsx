107ba9b939d16223ab78d71c39fa9a537be7086a
feliperromao <feliperomaocad@gmail.com>
2021-11-07 13:13:34 -0300

    navegação programatica

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,6 +1,22 @@
 import Link from 'next/link'
+import router, { useRouter } from 'next/router'
 
 export default function rotas() {
+
+    function navegacaoSimples(url) {
+        router.push(url)
+    }
+
+    function navegacaoComParams() {
+        router.push({
+            pathname: "/rotas/params",
+            query: {
+                id: '123456kkk',
+                name: 'Ana Bea'
+            }
+        })
+    }
+
     return (
         <div>
             <h1>Rotas Index</h1>
@@ -17,6 +33,12 @@ export default function rotas() {
                     <Link href="/rotas/1234/Felipe">Felipe (123)</Link>
                 </li>
             </ul>
+
+            <div style={{display: 'flex', flexDirection: 'column' , alignItems: 'flex-start'}}>
+                <button onClick={() => router.push('/rotas/1234/buscar')}>Buscar</button>
+                <button onClick={() => navegacaoSimples('/rotas/1234/Felipe')}>Felipe</button>
+                <button onClick={() => navegacaoComParams()}>Params</button>
+            </div>
         </div>
     )
 }
