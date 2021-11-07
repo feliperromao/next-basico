1d4bc2438935dde60244c30fefc48664c43e8f4b
feliperromao <feliperomaocad@gmail.com>
2021-11-07 12:30:53 -0300

    navegação utilizando link

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,9 +1,22 @@
+import Link from 'next/link'
+
 export default function rotas() {
     return (
         <div>
-            <h1>
-                Rotas Index
-            </h1>
+            <h1>Rotas Index</h1>
+            <ul>
+                <li>
+                    <Link href="/rotas/params?id=12&name=Ana">Ana (12)</Link>
+                </li>
+
+                <li>
+                    <Link href="/rotas/1234/buscar">Buscar (123)</Link>
+                </li>
+
+                <li>
+                    <Link href="/rotas/1234/Felipe">Felipe (123)</Link>
+                </li>
+            </ul>
         </div>
     )
 }
--- src/pages/rotas/params.jsx
+++ src/pages/rotas/params.jsx
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default () => {
     const router = useRouter()
-
     const { id, name } = router.query
 
     return (
@@ -10,6 +10,9 @@ export default () => {
             <h1>Rotas Params</h1>
             <h3>ID: {id}</h3>
             <h3>Nome: {name}</h3>
+            <Link href="/rotas">
+                <button>Voltar</button>
+            </Link>
         </div>
     )
 }
